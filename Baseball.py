2084bfe6064546276733adbeeb42517f6b77f101
Emily Curtis <emweecurt@Emilys-MacBook-Air.local>
2017-06-08 00:06:17 -0700

    Check for timeout

--- Baseball.py
+++ Baseball.py
@@ -1,17 +1,17 @@
 import requests
 import json
+import sys
 
-# url = 'http://gd2.mlb.com/components/game/mlb/year_2017/month_05/day_01/gid_2017_05_01_milmlb_slnmlb_1/boxscore.json'
-
-# rawData = requests.get(url)
-# print(rawData)
-# print(rawData.json())
-
-with open('sample_data.json') as data_file:
-    data = json.load(data_file)
-
-print(data)
+url = 'http://gd2.mlb.com/components/game/mlb/year_2017/month_05/day_01/gid_2017_05_01_milmlb_slnmlb_1/boxscore.json'
+try:
+    rawData = requests.get(url, timeout = 2)
+except requests.exceptions.Timeout:
+    print("Retrieval of data from website timed out.")
+    sys.exit()
+    
+data = rawData.json()
 
 pitch_data = data['data']['boxscore']['pitching']
 away_starter = pitch_data[0]['pitcher'][0]
 home_starter = pitch_data[1]['pitcher'][0]
+
