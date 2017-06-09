9d152d991303b8f564505bda077567e6bc3d2ea9
Emily Curtis <emweecurt@Emilys-MacBook-Air.local>
2017-06-09 00:35:23 -0700

    get_game_starters and fetch_boxscore functions

--- Baseball.py
+++ Baseball.py
@@ -2,16 +2,26 @@ import requests
 import json
 import sys
 
-url = 'http://gd2.mlb.com/components/game/mlb/year_2017/month_05/day_01/gid_2017_05_01_milmlb_slnmlb_1/boxscore.json'
-try:
-    rawData = requests.get(url, timeout = 2)
-except requests.exceptions.Timeout:
-    print("Retrieval of data from website timed out.")
-    sys.exit()
-    
-data = rawData.json()
+def get_game_starters(boxscore):
+    pitch_data = boxscore['data']['boxscore']['pitching']
+    away_starter = pitch_data[0]['pitcher'][0]
+    home_starter = pitch_data[1]['pitcher'][0]
+    return (away_starter, home_starter)
 
-pitch_data = data['data']['boxscore']['pitching']
-away_starter = pitch_data[0]['pitcher'][0]
-home_starter = pitch_data[1]['pitcher'][0]
+def fetch_boxscore(url):
+    try:
+        response = requests.get(url, timeout = 2)
+    except requests.exceptions.Timeout:
+        print("Retrieval of data from website timed out.")
+        return
+    if(response.status_code != 200):
+        return
+    return response.json()
+
+
+url = 'http://gd2.mlb.com/components/game/mlb/year_2017/month_05/day_01/gid_2017_05_01_milmlb_slnmlb_1/boxscore.json'
+data = fetch_boxscore(url)
+
+game_one = get_game_starters(data)
+print(game_one)
 
