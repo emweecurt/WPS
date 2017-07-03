014fa3b0352dad57c7461b218e24f36f8e3c82d4
Emily Curtis <elcurtis96@gmail.com>
2017-07-02 23:42:08 -0700

    Got all gids for a single day and looped through multiple days

--- Baseball.py
+++ Baseball.py
@@ -7,8 +7,19 @@ import datetime
 # returns pitching data for both starting pictchers in a tuple
 def get_game_starters(boxscore):
     pitch_data = boxscore['data']['boxscore']['pitching']
-    away_starter = pitch_data[0]['pitcher'][0]
-    home_starter = pitch_data[1]['pitcher'][0]
+
+    # check for multiple pitchers
+    # if there are multiple pitchers, take the first one
+    if type(pitch_data[0]['pitcher']) is list:
+        away_starter = pitch_data[0]['pitcher'][0]
+    else:
+        away_starter = pitch_data[0]['pitcher']
+
+    if type(pitch_data[1]['pitcher']) is list:
+        home_starter = pitch_data[1]['pitcher'][0]
+    else:
+        home_starter = pitch_data[1]['pitcher']
+
     return (away_starter, home_starter)
 
 # boxscore: the whole boxscore.json file from gd2.mlb.com for a single game
@@ -18,9 +29,9 @@ def get_game_runs(boxscore):
     away_team_runs = boxscore['data']['boxscore']['linescore']['away_team_runs']
     return (away_team_runs, home_team_runs)
 
-# url: the url to a boxscore.json file
-# returns the boxscore as a json object
-def fetch_boxscore(url):
+# url: the url to a json file
+# returns the file as a json object
+def fetch_json(url):
     try:
         response = requests.get(url, timeout = 2)
     except requests.exceptions.Timeout:
@@ -30,29 +41,70 @@ def fetch_boxscore(url):
         return
     return response.json()
 
+# date: date of game
+# gid: game id required for url
+# prints starters game scores and result of game
 def handle_single_game(date, gid):
+    print(gid)
     # Construct gd2 URL
     month_padded = date.strftime('%m')
     day_padded = date.strftime('%d')
-    url = 'http://gd2.mlb.com/components/game/mlb/year_' + str(date.year) + '/month_' + month_padded + '/day_' + day_padded + '/' + gid + '/boxscore.json'
+    url = f"http://gd2.mlb.com/components/game/mlb/year_{date.year}/month_{month_padded}/day_{day_padded}/{gid}/boxscore.json"
 
-    data = fetch_boxscore(url)
+    # retrieve json from url
+    data = fetch_json(url)
     if(data == None):
+        print("No data.", "\n")
         return
 
     (away_starter, home_starter) = get_game_starters(data)
 
+    # pull game scores
     away_starter_gs = away_starter["game_score"]
     home_starter_gs = home_starter["game_score"]
 
+    # print game scores
     print('Away game score:', away_starter_gs)
     print('Home game score:', home_starter_gs)
 
+    # pull game result
     (away_runs, home_runs) = get_game_runs(data)
     home_won = home_runs > away_runs
 
-    print('Home won?', home_won)
+    # print game result
+    print('Home won?', home_won, "\n")
 
-date = datetime.date(2017, 5, 1)
-gid = 'gid_2017_05_01_milmlb_slnmlb_1'
-handle_single_game(date, gid)
+# date: date of interest
+# returns list of gids for date provided
+def get_gids_for_day(date):
+    # Construct gd2 URL
+    month_padded = date.strftime('%m')
+    day_padded = date.strftime('%d')
+
+    url = f"http://gd2.mlb.com/components/game/mlb/year_{date.year}/month_{month_padded}/day_{day_padded}/miniscoreboard.json"
+
+    # retrieve json from url
+    data = fetch_json(url)
+    if(data == None):
+        return
+    
+    games = data["data"]["games"]["game"]
+
+    # pull gids and return
+    return ["gid_" + game["gameday_link"] for game in games]
+
+# date: date of interest
+# print results from all games in a single day
+def handle_single_day(date):
+    gids = get_gids_for_day(date)
+
+    for gid in gids:
+        handle_single_game(date, gid)
+
+start = datetime.date(2017, 5, 20)
+end = datetime.date(2017, 5, 20)
+delta = datetime.timedelta(days = 1)
+d = start
+while d <= end:
+    handle_single_day(d)
+    d += delta
